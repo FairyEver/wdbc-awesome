76cc96981520b39eb6c41cdd60a2a25a8346a3f2
FairyEver <1711467488@qq.com>
2020-01-18 18:07:10 +0800

    style

--- src/components/library-element/com.vue
+++ src/components/library-element/com.vue
@@ -1,20 +1,32 @@
 <style lang="scss">
 .library-element {
+  @extend .unselect;
+  @extend .radius4;
   position: relative;
+  transition: all .1s;
+  border-radius: 4px;
+  overflow: hidden;
+  padding: 2px;
+  background-color: #FFF;
+  &:hover {
+    transform: translateY(-2px);
+    box-shadow:
+      0 2px 4px 0 rgba(#000, 0.1),
+      0 2px 20px 0 rgba(#000, 0.20);
+  }
   .library-element--cover {
-    width: 100%;
-    padding-top: 100%;
-    height: 0;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
+    @extend .square;
+    @extend .bg-cover;
+    @extend .radius2;
   }
 }
 </style>
 
 <template>
   <div class="library-element">
-    <img class="library-element--cover" :style="{ backgroundImage: `url(${$url(value.cover)})` }"/>
+    <img
+      class="library-element--cover"
+      :style="{ backgroundImage: `url(${$url(value.cover)})` }"/>
   </div>
 </template>
 

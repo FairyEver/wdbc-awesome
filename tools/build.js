05c41a34f2ec752c81801afbb8f0f78b514ed147
FairyEver <1711467488@qq.com>
2020-01-18 15:56:44 +0800

    优化构建

--- tools/build.js
+++ tools/build.js
@@ -1,16 +1,21 @@
 (async () => {
+  const resolve = dir => path.join(__dirname, dir)
+  const path = require('path')
+  const write = require('./write')
+  const folder = resolve('../library')
+  // 环境变量
   const argv = process.argv.splice(2)
+  const { scan, maker } = require('./scan')
   if (argv.length !== 2) {
     console.log('没有设置 AK SK')
     return
   }
   global.AK = argv[0]
   global.SK = argv[1]
-  const path = require('path')
-  const resolve = dir => path.join(__dirname, dir)
-  const { scan, maker } = require('./scan')
-  const folder = resolve('../library')
-  const result = maker(await scan({ folderPath: folder }))
-  const write = require('./write')
-  await write(resolve('../build/materials.json'), JSON.stringify(result, null, 2))
+  // 物料
+  const materials = {}
+  // 扫描资料库
+  materials.library = maker(await scan({ folderPath: folder })).elements
+  // 生成文件
+  await write(resolve('../build/materials.json'), JSON.stringify(materials, null, 2))
 })()

94549dc2c184d0ff6232845867cb9397b96b945f
FairyEver <1711467488@qq.com>
2020-01-20 22:07:15 +0800

    文件夹图标显示

--- src/components/library-element/com.vue
+++ src/components/library-element/com.vue
@@ -6,19 +6,54 @@
   @extend .unselect;
   @extend .radius4;
   padding: $PADDING;
+  // 文件夹
+  .library-element--folder-group {
+    margin-bottom: $PADDING;
+    $ICON_FOLDER_MARGIN: 2px;
+    // 文件夹 行
+    .library-element--icon-folder-row {
+      margin-bottom: $ICON_FOLDER_MARGIN;
+      &:last-child {
+        margin-bottom: 0px;
+      }
+      // 文件夹 列
+      .library-element--icon-folder-col {
+        margin-right: $ICON_FOLDER_MARGIN;
+        padding: 2px;
+        background-color: rgba($COLOR_DARK, 0.03);
+        &:last-child {
+          margin-right: 0px;
+        }
+        // 文件夹 列 有效
+        &.library-element--icon-folder-col__has-image {
+          background-color: #FFF;
+        }
+        // 文件夹 列 中的 一个文件
+        .library-element--icon-folder {
+          @extend .radius1;
+          background-color: #FFF;
+          max-width: 100%;
+          max-height: 100%;
+        }
+      }
+    }
+  }
+  // 文件
   .library-element--icon-group {
     @extend .transition;
     @extend .radius2;
     margin-bottom: $PADDING;
     padding: 2px;
     background-color: #FFF;
-    .library-element--icon {
+    // 文件 一个项目
+    .library-element--icon-file {
       @extend .radius2;
       background-color: #FFF;
       max-width: 100%;
       max-height: 100%;
     }
   }
+  // 文件或者文件夹名称
   .library-element--title {
     font-size: 12px;
     .library-element--title-label {
@@ -30,13 +65,9 @@
       padding: 0 $PADDING;
     }
   }
+  // 鼠标移过的样式
   &:hover {
     background-color: rgba($COLOR_DARK, 0.05);
-    .library-element--icon-group {
-      box-shadow:
-        0 2px 4px 0 rgba($COLOR_DARK, 0.2),
-        0 2px 20px 0 rgba($COLOR_DARK, 0.1);
-    }
     .library-element--title {
       .library-element--title-label {
         background-color: #0058D0;
@@ -49,8 +80,25 @@
 
 <template>
   <div class="library-element" @click="onClick">
-    <square class="library-element--icon-group" flex="main:center cross:center">
-      <img class="library-element--icon" :src="$url(value.cover || value.url)">
+    <square v-if="isDir" class="library-element--folder-group" flex="dir:top main:justify box:mean">
+      <div v-for="row in 3" :key="row" class="library-element--icon-folder-row" flex="main:justify box:mean">
+        <div
+          v-for="col in 3"
+          :key="col"
+          class="library-element--icon-folder-col"
+          :class="{
+            'library-element--icon-folder-col__has-image': value.elements.length >= (3 * (row - 1) + col)
+          }"
+          flex="main:center cross:center">
+          <img
+            v-if="value.elements.length >= (3 * (row - 1) + col)"
+            class="library-element--icon-folder"
+            :src="$url(value.elements[3 * (row - 1) + col - 1].url, '/icon/folder.png')">
+        </div>
+      </div>
+    </square>
+    <square v-else class="library-element--icon-group" flex="main:center cross:center">
+      <img class="library-element--icon-file" :src="$url(value.cover || value.url)">
     </square>
     <div class="library-element--title" flex="main:center">
       <div class="library-element--title-label">
@@ -77,13 +125,13 @@ export default {
     }
   },
   computed: {
-    isFolder () {
+    isDir () {
       return isArray(this.value.elements)
     }
   },
   methods: {
     onClick () {
-      if (this.isFolder) {
+      if (this.isDir) {
         this.$store.commit('materials/viewPathPush', {
           label: this.value.name,
           value: this.index
--- src/plugins/library/index.js
+++ src/plugins/library/index.js
@@ -2,7 +2,8 @@ import store from '@/store'
 
 export default {
   install (Vue) {
-    Vue.prototype.$url = url => {
+    Vue.prototype.$url = (url = '', def = '') => {
+      if (!url) return def
       return store.getters['materials/libraryBase'] + store.getters['materials/libraryPrefix'] + url
     }
   }

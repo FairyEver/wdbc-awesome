d244c867ebb1941f2375454b91e56fca53a1d169
FairyEver <1711467488@qq.com>
2020-01-24 12:15:15 +0800

    简化样式

--- src/components/log-list/com.vue
+++ src/components/log-list/com.vue
@@ -2,7 +2,7 @@
 .log-list {
   @extend .unselect;
   margin: 0 -16px;
-  height: 200px;
+  height: 300px;
   width: 240px;
   overflow: scroll;
   .log-list-item {
@@ -13,32 +13,15 @@
     &.index-1 {
       opacity: 1.0;
       font-weight: bolder;
-      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 1.0); }
     }
-    &.index-2 {
-      opacity: 0.9;
-      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.9); }
-    }
-    &.index-3 {
-      opacity: 0.8;
-      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.8); }
-    }
-    &.index-4 {
-      opacity: 0.7;
-      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.7); }
-    }
-    &.index-5 {
-      opacity: 0.6;
-      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.6); }
-    }
-    &.index-6 {
-      opacity: 0.5;
-      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.5); }
-    }
+    &.index-2 { opacity: 0.9; }
+    &.index-3 { opacity: 0.8; }
+    &.index-4 { opacity: 0.7; }
+    &.index-5 { opacity: 0.6; }
+    &.index-6 { opacity: 0.5; }
     &:hover {
-      background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.4);
+      background-color: $COLOR_TEXT_LABEL_ACTIVE;
       color: #FFF;
-      opacity: 1.0;
     }
   }
 }

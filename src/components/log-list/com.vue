018a431faa88fc70183d72420e543624cdbf6ede
FairyEver <1711467488@qq.com>
2020-01-24 10:41:51 +0800

    优化消息列表显示

--- src/components/log-list/com.vue
+++ src/components/log-list/com.vue
@@ -9,9 +9,36 @@
     @extend .nowrap;
     font-size: 12px;
     padding: 5px 16px;
+    opacity: 0.4;
+    &.index-1 {
+      opacity: 1.0;
+      font-weight: bolder;
+      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 1.0); }
+    }
+    &.index-2 {
+      opacity: 0.9;
+      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.9); }
+    }
+    &.index-3 {
+      opacity: 0.8;
+      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.8); }
+    }
+    &.index-4 {
+      opacity: 0.7;
+      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.7); }
+    }
+    &.index-5 {
+      opacity: 0.6;
+      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.6); }
+    }
+    &.index-6 {
+      opacity: 0.5;
+      &:hover { background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.5); }
+    }
     &:hover {
-      background-color: $COLOR_TEXT_LABEL_ACTIVE;
+      background-color: rgba($COLOR_TEXT_LABEL_ACTIVE, 0.4);
       color: #FFF;
+      opacity: 1.0;
     }
   }
 }
@@ -21,8 +48,9 @@
   <div class="log-list">
     <div
       class="log-list-item"
-      v-for="item of $store.getters['log/list']"
-      :key="item.id">
+      v-for="(item, index) of $store.getters['log/list']"
+      :key="item.id"
+      :class="`index-${index + 1}`">
       {{ item.text }}
     </div>
   </div>

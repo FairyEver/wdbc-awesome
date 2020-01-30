34c59bf70dfe4548293934851e438501913640e5
FairyEver <1711467488@qq.com>
2020-01-30 12:17:55 +0800

    资料库

--- src/App.vue
+++ src/App.vue
@@ -13,29 +13,10 @@ html, body {
 </template>
 
 <script>
-// const { app } = require('electron').remote
-// const { DownloaderHelper } = require('node-downloader-helper')
-// const dl = new DownloaderHelper('https://qiniucdn.fairyever.com/20191215221254.png', app.getPath('userData'))
-// dl.on('end', () => console.log('Download Completed'))
-// dl.on('progress', (stats) => console.log(stats))
-// dl.start()
 export default {
   async created () {
     // 加载本地数据
     await this.$store.dispatch('materials/load')
-  },
-  async mounted () {
-    // await this.$store.dispatch('local/write', {
-    //   fileName: 'demo.json',
-    //   fileValue: '10'
-    // })
-    // const fileValue = await this.$store.dispatch('local/read', {
-    //   fileName: 'demo.json',
-    //   defaultValue: '{ "name": "liyang" }'
-    // })
-    // console.log(fileValue)
-    // const stop = await this.$store.dispatch('loading/start')
-    // stop()
   }
 }
 </script>
--- src/components/file-detail/com.vue
+++ src/components/file-detail/com.vue
@@ -1,15 +1,58 @@
 <style lang="scss">
-.file-detail {}
+.file-detail {
+  @extend .unselect;
+  max-width: 400px;
+  margin: 0 auto;
+  .preview {
+    width: 100%;
+  }
+  .v-lazy-image {
+    filter: blur(10px);
+    transition: filter 0.7s;
+  }
+  .v-lazy-image-loaded {
+    filter: blur(0);
+  }
+}
 </style>
 
 <template>
   <div class="file-detail">
-    file-detail
+    <v-lazy-image
+      class="preview"
+      ref="img"
+      :src="imageUrl"
+      :src-placeholder="imageUrlPlaceholder"/>
+    <div>{{ file.name }}</div>
+    <div>{{ file.width }} x {{ file.height }}</div>
+    <div>{{ size }}</div>
   </div>
 </template>
 
 <script>
+import url from '@/mixins/url.js'
+import byte from '@/utils/byte.js'
 export default {
-  name: 'file-detail'
+  name: 'file-detail',
+  mixins: [
+    url
+  ],
+  computed: {
+    file () {
+      return this.$store.getters['view/file']
+    },
+    filePath () {
+      return this.file.filePath ? 'file://' + this.file.filePath : ''
+    },
+    imageUrl () {
+      return this.url(this.file.url)
+    },
+    imageUrlPlaceholder () {
+      return this.url(this.file.url, '', 200)
+    },
+    size () {
+      return byte(this.file.size)
+    }
+  }
 }
 </script>

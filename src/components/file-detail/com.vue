19aa8d9ac8d5fd7d168737604a1d8ba1947216ae
FairyEver <1711467488@qq.com>
2020-01-31 11:26:36 +0800

    image-guide-use

--- src/components/file-detail/com.vue
+++ src/components/file-detail/com.vue
@@ -49,21 +49,6 @@
     color: rgba(#000, .45);
     margin-bottom: $LAYOUT_MAIN_PADDING;
   }
-  .image-guide {
-    @extend .unselect;
-    @extend .radius2;
-    padding: $LAYOUT_MAIN_PADDING * 2 0;
-    background-color: #FFF;
-    img {
-      display: block;
-      width: 180px;
-      height: 80px;
-      margin-bottom: $LAYOUT_MAIN_PADDING / 2;
-    }
-    .image-guide-label {
-      font-size: 12px;
-    }
-  }
 }
 </style>
 
@@ -81,10 +66,7 @@
       {{ file.width }} x {{ file.height }} {{ size }}
       <library-element-badge type="hdd"/>
     </div>
-    <div class="image-guide" flex="dir:top main:center cross:center">
-      <img src="/image/image-use-guide.png" draggable="false">
-      <div class="image-guide-label">请将图片拖拽至其它应用程序窗口使用</div>
-    </div>
+    <image-guide-use/>
   </div>
 </template>
 
---
+++ src/components/image-guide-use/com.vue
@@ -0,0 +1,30 @@
+<style lang="scss">
+.image-guide-use {
+  @extend .unselect;
+  @extend .radius2;
+  padding: $LAYOUT_MAIN_PADDING * 2 0;
+  background-color: #FFF;
+  img {
+    display: block;
+    width: 180px;
+    height: 80px;
+    margin-bottom: $LAYOUT_MAIN_PADDING / 2;
+  }
+  .image-guide-label {
+    font-size: 12px;
+  }
+}
+</style>
+
+<template>
+  <div class="image-guide-use" flex="dir:top main:center cross:center">
+    <img src="/image/image-use-guide.png" draggable="false">
+    <div class="image-guide-label">请将图片拖拽至其它应用程序窗口使用</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'image-guide-use'
+}
+</script>

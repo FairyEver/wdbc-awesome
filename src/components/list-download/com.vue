e52c1d87d1f399462212e013577a0b412ad942da
FairyEver <1711467488@qq.com>
2020-01-31 15:24:42 +0800

    资料库更新

--- src/components/list-download/com.vue
+++ src/components/list-download/com.vue
@@ -1,23 +1,18 @@
 <style lang="scss">
 .list-download {
   @extend .unselect;
-  .list-download-item {
-    @extend .nowrap;
-    margin-bottom: $LAYOUT_MAIN_PADDING / 2;
-    font-size: 12px;
-  }
 }
 </style>
 
 <template>
   <div class="list-download">
-    <div class="list-download-item" v-for="item of $store.getters['download/list']" :key="item.id">
-      <div flex="main:justify">
-        <div>{{ item.fileName }}</div>
-        <div>{{ item.downloaded }} / {{ item.total }}</div>
-      </div>
-      <a-progress :percent="item.progress" size="small" />
-    </div>
+    <progress-bar
+      v-for="item of $store.getters['download/list']"
+      :key="item.id"
+      :name="item.fileName"
+      :downloaded="item.downloaded"
+      :total="item.total"
+      :percent="item.progress"/>
   </div>
 </template>
 
---
+++ src/components/progress-bar/com.vue
@@ -0,0 +1,15 @@
+<style lang="scss">
+.progress-bar {}
+</style>
+
+<template>
+  <div class="progress-bar">
+    progress-bar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'progress-bar'
+}
+</script>

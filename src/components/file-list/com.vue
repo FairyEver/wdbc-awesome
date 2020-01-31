1a1b46b7588f8f74ce8674164ba51cdb5cdf4457
FairyEver <1711467488@qq.com>
2020-01-31 11:30:54 +0800

    file-list

---
+++ src/components/file-list/com.vue
@@ -0,0 +1,28 @@
+<style lang="scss">
+.file-list {}
+</style>
+
+<template>
+  <a-row class="file-list" style="margin: 0 -10px;">
+    <a-col v-for="(el, index) of $store.getters['view/list']" :key="el.id" v-bind="colSetting">
+      <library-element :value="el" :index="index"/>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+export default {
+  name: 'file-list',
+  data () {
+    return {
+      colSetting: {
+        xs: 8,
+        sm: 6,
+        md: 4,
+        lg: 3,
+        xl: 2
+      }
+    }
+  }
+}
+</script>
--- src/views/album/index.vue
+++ src/views/album/index.vue
@@ -6,7 +6,7 @@
     padding: $LAYOUT_MAIN_PADDING;
     padding-top: $LAYOUT_HEADER_HEIGHT;
   }
-  .page-album--layer-breadcrumb {
+  .page-album--layer-header {
     position: absolute;
     top: 0px;
     left: 0px;
@@ -19,13 +19,9 @@
   <div class="page-album">
     <div class="page-album--layer-main">
       <file-detail v-if="$store.getters['view/file']"/>
-      <a-row v-else style="margin: 0 -10px;">
-        <a-col v-for="(el, index) of $store.getters['view/list']" :key="el.id" v-bind="colSetting">
-          <library-element :value="el" :index="index"/>
-        </a-col>
-      </a-row>
+      <file-list v-else/>
     </div>
-    <div class="page-album--layer-breadcrumb">
+    <div class="page-album--layer-header">
       <album-breadcrumb/>
     </div>
   </div>
@@ -33,17 +29,6 @@
 
 <script>
 export default {
-  name: 'page-album',
-  data () {
-    return {
-      colSetting: {
-        xs: 8,
-        sm: 6,
-        md: 4,
-        lg: 3,
-        xl: 2
-      }
-    }
-  }
+  name: 'page-album'
 }
 </script>
